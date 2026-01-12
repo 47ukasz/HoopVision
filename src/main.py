6305fd7f33ad121fc33ffb54471125933372a72e
Łukasz Janus <lukaszjanus@Mac-mini-ukasz.local>
2026-01-12 13:12:04 +0100

    fixed net moved detection

--- src/main.py
+++ src/main.py
@@ -3,18 +3,20 @@ import pandas as pd
 import joblib
 import cv2 as cv
 from ultralytics import YOLO
+
 from video_analyzing_scripts.feature_engineering import feature_extraction
-from video_analyzing_scripts.rim import handle_detect_net_moved, handle_draw_net_moved, handle_draw_rim, handle_detect_rim, handle_calculate_ball_in_rim
+from video_analyzing_scripts.rim import handle_detect_net_moved, handle_draw_rim, handle_detect_rim, handle_calculate_ball_in_rim
 from video_analyzing_scripts.trajectory import handle_detect_trajectory_point, handle_draw_trajectory, handle_draw_ball
 from video_analyzing_scripts.throw_roi import create_tracking_roi, is_ball_in_roi
 from constants import RIM_CLASS_INDEX, MIN_RIM_CONF, BASKETBALL_CLASS_INDEX, MIN_BALL_CONF
+
 project_dir = Path.cwd()
 model_path = project_dir / "models/hoopvision_v7/weights/best.pt"
 lr_model_path = project_dir / "models/lr_model.pkl"
-# video_path = project_dir / "data/collection/trafione/scored13.mp4"
+# video_path = project_dir / "data/collection/trafione/scored2.mp4"
 # video_path = project_dir / "data/collection/odbite_od_tablicy/backhit27.mp4"
-# video_path = project_dir / "data/collection/nietrafione/missed25.mp4"
-video_path = project_dir / "data/collection/videos/test/video12.mp4"
+video_path = project_dir / "data/collection/nietrafione/missed19.mp4"
+# video_path = project_dir / "data/collection/videos/test/video9.mp4"
 output_path = project_dir / "analysis_results" / f"{video_path.stem}_analyzed.mp4"
 
 model = YOLO(model_path)
@@ -33,8 +35,14 @@ frame_h = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
 out = cv.VideoWriter(str(output_path), fourcc, fps, (frame_w, frame_h))
 
+# stan rzutu (to nowe)
+# IDLE, TRACKING, WAIT_NET, COOLDOWN
+shotState = "IDLE" # zmienna do stanu rzutu, giga wazna bo wczesniej cooldown odcinal detekcje czy sie siatka ruszyla
+
 # zmienne do określenia trajektorii ruchu piłki
 trajectory_points = []
+last_ball_xyxy = None
+last_point = None
 frame_count = 0
 prev_frame = None
 point = None
@@ -42,18 +50,18 @@ point = None
 ## zmienne do logiki rzutow
 shot_cooldown = 0 #licznik przerwy miedzy rzutami
 COOLDOWN_DURATION = int(fps * 2) #przerwa miedzy sledzeniem
-is_tracking_shot = False #czy sledzenie jest wlaczone
 tracking_roi = None # obszar region of intrest
 total_shots = 0
 
 # zmienne do detekcji ruchu siatki
+ball_in_rim_prev = False
 net_moved_detection_cooldown = 0
 shot_net_moved = False
 NET_MOVED_DETECTION_DURATION = int(fps * 0.2) # około 200ms 
-basketball_state = "in_air" # "in_air" -> rzut, "in_rim" -> jest w bounding boxie siatki, "out_rim" -> piłka wyszła z bb siatki
 net_moved_in_frames = [] # tablica która będzie przechowywała bool'a czy dla danych klatek zaobserwowano ruch siatki
 net_detection_started = False
 ball_center_points_in_rim = [] # tablica potrzebna do obliczenia najmniejszej odległości od środa obręczy, na tej podstawie będzie obliczany wspólczynnik ruchu siatki
+wait_net_start = False
 
 # zmienne do detekcji kosza oraz wybrania prawidłowego
 detected_rim_box = None #[xmin, ymin, xmax, ymax]
@@ -68,26 +76,14 @@ while cap.isOpened():
 
     frame_count += 1
     clean_frame = frame.copy()
-    
+
     # licznik rzutow
     cv.rectangle(frame, (0, 0), (250, 60), (0, 0, 0), -1)
     cv.putText(frame, f"Rzuty: {total_shots}", (20, 40), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
-    # logika przerwy sledzenia
-    if shot_cooldown > 0:
-        shot_cooldown -= 1
-        cv.putText(frame, f"Cooldown: {shot_cooldown}", (20, 100), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
- 
-        # rysuje strefe na szaro(nieaktywne)
-        if tracking_roi:
-            cv.rectangle(frame, (tracking_roi[0], tracking_roi[1]), (tracking_roi[2], tracking_roi[3]), (100, 100, 100), 2)
-        
-        out.write(frame)
-        cv.imshow("Analiza rzutu", frame)
-        if cv.waitKey(1) == ord('q'): 
-            break
-        continue
-
+    # stan rzutu
+    cv.putText(frame, f"STATE: {shotState}", (20, 95), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 255), 2)
+    
     # detekcja
     results = model(frame, verbose=False, imgsz=640)
     boxes = results[0].boxes
@@ -115,6 +111,13 @@ while cap.isOpened():
                 ball_xyxy = box.xyxy[0].cpu().numpy()
                 ball_candidates.append((point, ball_xyxy))
 
+    # jak yolo nie wykryje pileczki w klatce to zawsze mamy ostatnia, przez co zredukuje sie liczba dziwnych przeskokow
+    if ball_candidates:
+        last_point, last_ball_xyxy = ball_candidates[-1]
+
+    if last_ball_xyxy is not None:
+        handle_draw_ball(frame, last_ball_xyxy)
+
     # tutaj wybieramy ten którego pole jest największe, czyli teoretycznie będzie bliżej na filmiku 
     if not rim_locked and rim_candidates:
         detected_rim_box, detected_rim_yolo_box = handle_detect_rim(rim_candidates)
@@ -125,122 +128,136 @@ while cap.isOpened():
         handle_draw_rim(frame, detected_rim_box)
         tracking_roi = create_tracking_roi(detected_rim_box, frame.shape)
     
-    # detekcja ruchu siatki tylko dla wybranego kosza
-    if detected_rim_yolo_box is not None and detected_rim_box is not None:
-        b_xyxy = None
-
-        if len(ball_candidates) > 0:
-            _, b_xyxy = ball_candidates[-1]
-
-        if b_xyxy is not None:
-            in_rim = handle_calculate_ball_in_rim(detected_rim_box, b_xyxy)
-            
-            if in_rim and basketball_state == "in_air":
-                basketball_state = "in_rim"
-            elif basketball_state == "in_rim":
-                ball_center_points_in_rim.append(b_xyxy)
-                print("Dodano")
-            elif not in_rim and basketball_state == "in_rim":
-                basketball_state = "out_rim"
-                net_moved_detection_cooldown = NET_MOVED_DETECTION_DURATION
-                net_moved_in_frames = [] 
-                net_detection_started = False
-
-        if basketball_state == "out_rim":
-            if not net_detection_started and b_xyxy is not None:
-                bx1, by1, bx2, by2 = b_xyxy
-                ball_center_y = (by1 + by2) * 0.5
-
-                rx1, ry1, rx2, ry2 = detected_rim_box
-                rim_bottom_y = ry2
-
-                if ball_center_y > rim_bottom_y:
-                    net_detection_started = True
-
-            if net_moved_detection_cooldown > 0 and net_detection_started:
-                net_moved_detection_cooldown -= 1
-                net_attr = handle_detect_net_moved(clean_frame, prev_frame, detected_rim_yolo_box, b_xyxy, ball_center_points_in_rim)
-
-                if net_attr is not None:
-                    is_moving, _ = net_attr
-                    net_moved_in_frames.append(bool(is_moving))
-                else:
-                    net_moved_in_frames.append(False)
-
-            if net_moved_detection_cooldown == 0 and len(net_moved_in_frames) > 0:
-                count_of_moved_nets = sum(net_moved_in_frames)
-                required_moves = 2 if fps <= 35 else 3
-                print(f"Liczba ruchów: {count_of_moved_nets}")
-                print(f"Wymagana liczba ruchów: {required_moves}")
-
-                shot_net_moved = count_of_moved_nets >= required_moves
-                basketball_state = "in_air"
-
-    # logika piłki, tylko teraz nie obliczam tutaj current_ball_box tylko xyxy piłki jest przekazywane do ball candidates
-    for point, current_ball_box in ball_candidates:
-        # sprawdzamy czy pilka w ROI
-        in_zone = tracking_roi and is_ball_in_roi(current_ball_box, tracking_roi)
-        handle_draw_ball(frame, current_ball_box)
-
-        if in_zone:
-            if not is_tracking_shot:
-                is_tracking_shot = True
-                trajectory_points = [] # reset dla nowego rzutu
-            trajectory_points.append(point)
-        else:
-            # Sprawdzenie czy pilka poza strefa -> koniec rzutu
-            if is_tracking_shot:
-                if basketball_state == "out_rim" and net_moved_detection_cooldown > 0:
-                    continue
-                
-                total_shots += 1
-                print(f"Rzut nr {total_shots} zakończony. Analiza...")
-
-                features = feature_extraction(trajectory_points, detected_rim_box, fps)
-                features["net_moved"] = shot_net_moved
-                features_df = pd.DataFrame([features])
-                model_input = features_df[["min_odleglosc_pix", "czy_w_tunelu_pod_obrecza"]]
-
-                prediction = lr_model.predict(model_input)[0]
-                if prediction == 'hit':
-                    result_text = "TRAFIONY!"
-                    color = (0, 255, 0) 
-                else:
-                    result_text = "PUDŁO"
-                    color = (0, 0, 255) 
-
-                print(f"Wynik LR: {result_text}")
-                print(f"Dane wejściowe: Dystans={features['min_odleglosc_pix']} | Tunel={features['czy_w_tunelu_pod_obrecza']} | Siatka={features["net_moved"]}")
-
-                # Reset
-                is_tracking_shot = False
-                shot_net_moved = False
-                shot_cooldown = COOLDOWN_DURATION
-                trajectory_points = []
-
-                net_moved_detection_cooldown = 0
-                basketball_state = "in_air" 
-                net_moved_in_frames = []
-                net_detection_started = False
-        
-        # specjalnie ogranicze do jednej piłki narazie
-        break
-
-    # rysowanie ROI
+    # rysowanie roi
     if tracking_roi:
-        color = (0, 255, 0) if is_tracking_shot else (255, 0, 0) # Zielony jak nagrywa, niebieski jak czuwa
+        color = (0, 255, 0) if shotState == "TRACKING" else (255, 0, 0) # Zielony jak nagrywa, niebieski jak czuwa
         cv.rectangle(frame, (tracking_roi[0], tracking_roi[1]), (tracking_roi[2], tracking_roi[3]), color, 2)
         cv.putText(frame, "ROI", (tracking_roi[0], tracking_roi[1]-10), cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
 
-    # if net_attr is not None:
-    #     handle_draw_net_moved(frame, net_attr[0], net_attr[1])
-    
-    cv.putText(frame, f"State: {basketball_state}", (20, 140), cv.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 0), 2)
-    if basketball_state == "out_rim":
-        cv.putText(frame, f"OutRimTimer: {net_moved_detection_cooldown}", (20, 170), cv.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 0), 2)
-
-    if is_tracking_shot:
+    if shotState == "TRACKING" and trajectory_points:
         handle_draw_trajectory(frame, trajectory_points)
+
+    # tutaj zbieramy punkty pilki gdy byla w obreczy, po to zeby obliczyc dystans od srodka i na bazie jego dobrac minialny wspolczynik ruchu siatki
+    ball_in_rim_now = False
+    if detected_rim_box is not None and last_ball_xyxy is not None:
+        ball_in_rim_now = handle_calculate_ball_in_rim(detected_rim_box, last_ball_xyxy)
+
+    if ball_in_rim_now and last_ball_xyxy is not None:
+        bx1, by1, bx2, by2 = last_ball_xyxy
+        cx = (bx1 + bx2) * 0.5
+        cy = (by1 + by2) * 0.5
+        ball_center_points_in_rim.append((frame_count, cx, cy))
+
+    # zmienne do okreslenia czy pilka byla w siatce i ja opuscila
+    left_rim_event = ball_in_rim_prev is True and ball_in_rim_now is False
+    ball_in_rim_prev = ball_in_rim_now
+
+    # jezeli pileczka byla w koszu to odpalamy nowy stan dla detekcji siatki i resetujemy zmienne gdyby to nie byl pierwszy rzut
+    if left_rim_event:
+        wait_net_start = True
+        net_moved_detection_cooldown = NET_MOVED_DETECTION_DURATION
+        net_moved_in_frames = []
+        net_detection_started = False
+
+        if shotState == "TRACKING":
+            shotState = "WAIT_NET"
+
+    # teraz tutaj jest core jak cos, obsluga stanow dla rzutu, moze 4 w sumie przyjmowac: IDLE - STAN MIEDZY RZUTAMI, TRACKING - SLEDZI PILKE W ROI, WAIT_NET - OBLICZA CZY SIE SIATKA RUSZYLA I TEZ PO RZUCIE DOKONUJE PREDYCKJI, COOLDOWN - PRZERWA DO KOLEJNEGO RZUTU
+    in_tracking_roi = False
+    if tracking_roi is not None and last_ball_xyxy is not None:
+        in_tracking_roi = is_ball_in_roi(last_ball_xyxy, tracking_roi)
+    
+    if shotState == "IDLE":
+        # to jest stan miedzy cooldownem a nowym rzutem, sluzy do resetowania zmiennych etc.
+        if shot_cooldown <= 0 and in_tracking_roi:
+            trajectory_points = []
+            ball_center_points_in_rim = []
+            shotState = "TRACKING" # przejscie do stany sledzenia pilki w roi
+
+            if last_point is not None:
+                trajectory_points.append(last_point)
+    elif shotState == "TRACKING":
+        # stan gdy pilka jest w roi
+        if in_tracking_roi:
+            # standardowo, gdy pileczka jest w roi dodajemy punkt do trajektorii
+            if last_point is not None:
+                trajectory_points.append(last_point)
+    elif shotState == "WAIT_NET":
+        # i to jest teraz stan do detekcji siatki, bugfixuje to ze wczesniej gdy pilka wypadala z roi to od razu byl cooldown i nie bylo czasu zeby sprawdzic czy sie siatka ruszyla
+        if wait_net_start and last_ball_xyxy is not None and detected_rim_box is not None:
+            # obliczamy czy pilka jest ponizej kosza, jezeli tak zaczynamy detekcje
+            
+            bx1, by1, bx2, by2 = last_ball_xyxy
+            ball_center_y = (by1 + by2) * 0.5
+
+            rx1, ry1, rx2, ry2 = detected_rim_box
+            rim_bottom_y = ry2
+
+            if ball_center_y > rim_bottom_y:
+                net_detection_started = True
+        
+        # czas w ktorym zbieramy dane do okreslenia czy sie siatka ruszyla
+        if net_moved_detection_cooldown > 0:
+            net_moved_detection_cooldown -= 1
+
+            if net_detection_started and detected_rim_yolo_box is not None and prev_frame is not None:
+                net_attr = handle_detect_net_moved(clean_frame, prev_frame, detected_rim_yolo_box, last_ball_xyxy, ball_center_points_in_rim)
+
+                if net_attr is not None:
+                    is_moving, _ = net_attr
+                    net_moved_in_frames.append(is_moving)
+                else:
+                    net_moved_in_frames.append(False)
+        
+        # koniec zbierania danych, wiec sprawdzamy czy siatka ruszyla sie wystarczajaca ilosc razy
+        if net_moved_detection_cooldown == 0:
+            count_of_moved_nets = sum(net_moved_in_frames)
+            required_moves = 2 if fps <= 35 else 3
+            print(f"Liczba ruchów: {count_of_moved_nets}")
+            print(f"Wymagana liczba ruchów: {required_moves}")
+
+            shot_net_moved = count_of_moved_nets >= required_moves
+
+            # to jest juz etap po rzucie wiec teraz jest predykcja
+
+            total_shots += 1
+            print(f"Rzut nr {total_shots} zakończony. Analiza...")
+            
+            features = feature_extraction(trajectory_points, detected_rim_box, fps)
+            features["net_moved"] = shot_net_moved
+            
+            features_df = pd.DataFrame([features])
+            model_input = features_df[["min_odleglosc_pix", "czy_w_tunelu_pod_obrecza"]]
+            prediction = lr_model.predict(model_input)[0]
+
+            if prediction == 'hit':
+                result_text = "TRAFIONY!"
+            else:
+                result_text = "PUDŁO"
+
+            print(f"Wynik LR: {result_text}")
+            print(f"Dane wejściowe: Dystans={features['min_odleglosc_pix']} | Tunel={features['czy_w_tunelu_pod_obrecza']} | Siatka={features["net_moved"]}")
+
+            # RESET
+
+            trajectory_points = []
+            net_moved_detection_cooldown = 0
+            net_moved_in_frames = []
+            net_detection_started = False
+            wait_net_start = False
+            ball_in_rim_prev = False
+            ball_center_points_in_rim = []
+
+            # ustawienie cooldownu po rzucie
+
+            shot_cooldown = COOLDOWN_DURATION
+            shotState = "COOLDOWN"
+    elif shotState == "COOLDOWN":
+        if shot_cooldown > 0:
+            shot_cooldown -= 1
+            cv.putText(frame, f"Cooldown: {shot_cooldown}", (20, 130), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)        
+        else:
+            shotState = "IDLE" # przejscie do stanu miedzy rzutami
 
     out.write(frame)
     cv.imshow("Analiza rzutu", frame)
--- src/video_analyzing_scripts/rim.py
+++ src/video_analyzing_scripts/rim.py
@@ -14,7 +14,7 @@ def handle_detect_net_moved(currentFrame, prevFrame, rim_box, b_xyxy = None, bal
     box_conf_level = float(rim_box.conf[0])
 
     padding = 10
-    DEFAULT_MIN_RIM_MOTION = 1.0
+    DEFAULT_MIN_RIM_MOTION = 0.5
 
     if box_class_index != RIM_CLASS_INDEX:
         return (False, None)
@@ -47,18 +47,19 @@ def handle_detect_net_moved(currentFrame, prevFrame, rim_box, b_xyxy = None, bal
     flow = cv.calcOpticalFlowFarneback(prev_area, curr_area, None, pyr_scale = 0.5, levels = 5, winsize = 11, iterations = 5, poly_n = 7, poly_sigma = 1.1, flags = 0)
     mag, _ = cv.cartToPolar(flow[:,:,0], flow[:,:,1])
 
-    # full_mask = handle_make_mask(currentFrame, rim_box, b_xyxy)
-    # mask_roi = full_mask[y1:y2, x1:x2]
-    # valid = (mask_roi == 255)
-
-    # if not np.any(valid):
-        # rim_motion_value = 0.0
-    # else:
     rim_motion_value = np.mean(mag)
 
     distance = handle_calculate_distance_from_rim_center(map(int, rim_box.xyxy[0]), ball_points)
 
+    if distance >= 0 and distance <= 15:
+        DEFAULT_MIN_RIM_MOTION = 0.25
+
+    if distance > 15 and distance <= 35:
+        DEFAULT_MIN_RIM_MOTION = 0.35
+
+
     print(f"Obliczony dystans: {distance}")
+    print(f"Rim motion value: {rim_motion_value}")
 
     if distance is not None:
         rim_moving = rim_motion_value > DEFAULT_MIN_RIM_MOTION
@@ -67,44 +68,6 @@ def handle_detect_net_moved(currentFrame, prevFrame, rim_box, b_xyxy = None, bal
 
     return (rim_moving, cords)
 
-def handle_make_mask(frame, net_box, b_xyxy = None, expand=0.6):
-    height, width = frame.shape[0:2]
-    
-    xyxy = net_box.xyxy[0].cpu().numpy()
-    x1, y1, x2, y2 = map(int, xyxy)
-
-    mask = np.zeros((height, width), dtype=np.uint8)
-
-    cv.rectangle(mask, (x1, y1), (x2, y2), 255, -1)
-
-    if b_xyxy is not None:
-        # dla bounding box'a piłki obliczamy jego środek oraz powiększamy go o expand %
-        # zabezpieczamy się w ten sposób, że piksele poza bounding box'em piłki jeszcze zawierają ślad przemieszczania piłki
-        bx1, by1, bx2, by2 = map(int, b_xyxy)
-
-        b_width = bx2 - bx1
-        b_height = by2 - by1
-
-        b_center_x = (bx1 + bx2) * 0.5
-        b_center_y = (by1 + by2) * 0.5
-
-        b_width_expanded = b_width * (1.0 + expand)
-        b_height_expanded = b_height * (1.0 + expand)
-
-        bx1_expanded = int(b_center_x - b_width_expanded * 0.5)
-        by1_expanded = int(b_center_y - b_height_expanded * 0.5)
-        bx2_expanded = int(b_center_x + b_width_expanded * 0.5)
-        by2_expanded = int(b_center_y + b_height_expanded * 0.5)
-
-        # zabezpiecznie, żeby nie wyrzuciło ujemnych współrzędnych albo spoza zakresu (width, height)
-        bx1_expanded = max(0, bx1_expanded)
-        by1_expanded = max(0, by1_expanded)
-        bx2_expanded = min(width - 1, bx2_expanded)
-        by2_expanded = min(height - 1, by2_expanded)
-
-        cv.rectangle(mask, (bx1_expanded, by1_expanded), (bx2_expanded, by2_expanded), 0, -1)
-
-    return mask
 
 def handle_draw_net_moved(currentFrame, rim_moved, cords):
     x1, y1, x2, y2 = cords
